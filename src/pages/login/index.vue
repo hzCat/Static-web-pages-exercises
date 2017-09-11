9b00c0bf813af3fcaf9d3fdb6cef13c9484aa061
hzCat <q302144074@hotmail.com>
2017-09-11 10:57:38 +0800

    login static comp

--- src/pages/login/index.vue
+++ src/pages/login/index.vue
@@ -1,7 +1,15 @@
 <template>
     <div class="my-login">
         <div class="bg-img"></div>
-        
+        <div class="main">
+            <h1 class="title">Health</h1>
+            <div class="form-group">
+                <input type="text" name="" class="login-input" value="131-2321-2341">
+                <input type="password" name="" class="login-input" placeholder="请输入密码">
+                <el-button type="success">登录</el-button>
+            </div>
+            <div class="connect">联系我们</div>
+        </div>
     </div>
 </template>
 
